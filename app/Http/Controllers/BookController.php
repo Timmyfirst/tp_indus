c0855f125f15d4ca8a66ee3da501243ad230a3e8
Timmyfirst <timmyc71@gmail.com>
2017-11-23 15:14:44 +0000

    consctruction photoshop 27

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -11,79 +11,10 @@ use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-     public function show($id) {
+    public function show($id) {
       //  return view('book', ['user' => User::findOrFail($id)]);
        return view('book', ['book' => Book::findOrFail($id)]);
-     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id) {
-        //
     }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id) {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id) {
-        //
-    }
-
 
     public function postForm(Request $request) {
       DB::table('books')->insert(array(
@@ -114,6 +45,4 @@ class BookController extends Controller
         Book::where('id', $request->input('id_supp'))->delete();
         return redirect()->back();
     }
-
-
 }
