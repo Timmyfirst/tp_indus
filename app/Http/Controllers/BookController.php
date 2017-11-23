0790bf872037e8d6b18bf24f08f5a8444645f504
Timmyfirst <timmyc71@gmail.com>
2017-11-23 15:31:04 +0000

    correctif phpcbs

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -11,37 +11,42 @@ use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    public function show($id) {
-      //  return view('book', ['user' => User::findOrFail($id)]);
-       return view('book', ['book' => Book::findOrFail($id)]);
+    public function show($id)
+    {
+       //  return view('book', ['user' => User::findOrFail($id)]);
+        return view('book', ['book' => Book::findOrFail($id)]);
     }
 
-    public function postForm(Request $request) {
-      DB::table('books')->insert(array(
-          'nom' => $request->input('nom'),
-          'auteur' => $request->input('auteur'),
-          'description' => $request->input('description'),
-          'genre' => $request->input('genre'),
-
-      ));
-      return redirect()->back();
+    public function postForm(Request $request)
+    {
+        DB::table('books')->insert(
+            array(
+            'nom' => $request->input('nom'),
+            'auteur' => $request->input('auteur'),
+            'description' => $request->input('description'),
+            'genre' => $request->input('genre')
+            )
+        );
+          return redirect()->back();
     }
 
 
-    public function recupBook() {
-          $book = DB::table('books')->distinct()->paginate(5);
-          $links = $book->setPath('')->render();
+    public function recupBook()
+    {
+        $book = DB::table('books')->distinct()->paginate(5);
+        $links = $book->setPath('')->render();
 
-      return view('book',['books'=>$book,'link'=>$links]);
+        return view('book', ['books'=>$book,'link'=>$links]);
     }
 
-    public function editBook(Request $request){
+    public function editBook(Request $request)
+    {
         Book::where('id', $request->input('id_edit'))->update(['nom' => $request->input('book_rename')]);
         return redirect()->back();
     }
 
-    public function deleteBook(Request $request) {
-
+    public function deleteBook(Request $request)
+    {
         Book::where('id', $request->input('id_supp'))->delete();
         return redirect()->back();
     }
